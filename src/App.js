d9a18b0f62e0fd8a5c53d426a0a0781cc9dd2e7d
levan mchedlishvili <golocogo4@gmail.com>
2022-06-01 01:33:39 +0400

    advice generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+
+const [advice,setAdvice] = useState({});
+const url = 'https://api.adviceslip.com/advice'
+
+const getAdvice = () => {
+  fetch(url,{
+    method:'GET',
+    cache: 'no-cache'
+}).then(response => response.json())
+  .then(data => {
+    setAdvice(data)
+  })
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+
+        {typeof advice.slip === 'undefined' ? (
+          <>
+            <div className='advice-container'>
+                  <p className='start-p'>Please enter button to get advice</p>
+            </div>
+            <button className='btn' onClick={getAdvice}><i className='fas fa-dice-five'></i></button>
+          </>
+            
+        ):(
+          <>
+              <div className='advice-container'>
+                <p className='advice-number'>Advice # {advice.slip.id}</p>
+                <p className='advice'>{advice.slip.advice}</p>
+              </div>
+              <button className='btn' onClick={getAdvice}><i className="fas fa-dice-five"></i></button>
+          </>
+        )}
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
